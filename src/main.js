2e0714b0ea0414420226d2afc9467092990c55bf
Wei-Chung Cheng <weichung.cheng@galileo.edu>
2025-03-12 08:37:16 -0600

    2nd commit. Implemented GET /books/:id endpoint.

--- src/main.js
+++ src/main.js
@@ -4,8 +4,19 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-let books = []
+let books = [{
+  id: 1,
+  title: "Omniscient Reader's Viewpoint",
+  description: "Greatest novel book."
+}];
 
 app.get("/books", (_req, res) => {
   res.json(books);
+});
+
+app.get("/books/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const book = books.find((b) => b.id === id);
+  if (!book) return res.status(404).json({ error: "Book not found." });
+  res.json(book);
 });
